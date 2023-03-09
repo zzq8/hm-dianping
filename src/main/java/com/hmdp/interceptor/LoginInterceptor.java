d4ace806632ca1e45e130a5b8dc98f2b73fed4b7
XD <547061946@qq.com>
2023-03-09 09:10:21 +0800

    P35

---
+++ src/main/java/com/hmdp/interceptor/LoginInterceptor.java
@@ -0,0 +1,29 @@
+package com.hmdp.interceptor;
+
+import com.hmdp.utils.UserHolder;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 三个可实现的方法
+ * 1）方法前
+ * 2）方法后
+ * 3）页面渲染后
+ */
+public class LoginInterceptor implements HandlerInterceptor {
+    //处理登录信息
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        // 1.判断是否需要拦截（ThreadLocal中是否有用户）
+        if (UserHolder.getUser() == null) {
+            // 没有，需要拦截，设置状态码
+            response.setStatus(401);
+            // 拦截
+            return false;
+        }
+        // 有用户，则放行
+        return true;
+    }
+}
---
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -0,0 +1,29 @@
+package com.hmdp;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.servlet.http.HttpServletRequest;
+
+@SpringBootTest
+class HmDianPingApplicationTests {
+
+    public static String getIpAddress(HttpServletRequest request) {
+
+        String ipAddress = request.getHeader("X-Forwarded-For");
+
+        if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getHeader("Proxy-Client-IP");
+        }
+
+        if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getHeader("WL-Proxy-Client-IP");
+        }
+
+        if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getRemoteAddr();
+        }
+
+        return ipAddress;
+    }
+
+}
